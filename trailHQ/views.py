9d4f1fb2ae848f32b3e2ec2eda1bb6601feaff13
hwoods01@ksu.edu <hwoods01@ksu.edu>
2017-03-15 15:24:55 -0500

    Getting detail pages working

--- trailHQ/views.py
+++ trailHQ/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import SingletracksTrail
+#from trailHQ.apiReq import  THIS WILL BE USED WHEN i HAVE CALLS TO MAKE
 
 # Create your views here.
 
 def trail_detail(request, pk):
+    try:
+        trail = SingletracksTrail.objects.get( pk= pk)
+        return render(request, 'trailHQ/trail_detail.html', {'trail': trail})
+    except SingletracksTrail.DoesNotExist:
 
-    trail = get_object_or_404(SingletracksTrail, pk)
-    return render(request, 'trailHQ/trail_detail.html',  {'trail': trail })
+
+
 
 
 def all(request):
