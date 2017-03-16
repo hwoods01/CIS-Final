9eb2277026540ee969ca479f340743c6586866b5
hwoods01@ksu.edu <hwoods01@ksu.edu>
2017-03-15 21:58:10 -0500

    Rudimentary searching with strings completed. Can display info found from db

---
+++ trailHQ/utils/ST_Helper.py
@@ -0,0 +1,5 @@
+
+
+
+
+#ef makeRequest(key):
--- trailHQ/views.py
+++ trailHQ/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from .models import SingletracksTrail
-#from trailHQ.apiReq import  THIS WILL BE USED WHEN i HAVE CALLS TO MAKE
-
+#from utils import ST_Helper #THIS WILL BE USED WHEN i HAVE CALLS TO MAKE
+from django.http import Http404
+from django.template import RequestContext
 # Create your views here.
 
 def trail_detail(request, pk):
     try:
-        trail = SingletracksTrail.objects.get( pk= pk)
-        return render(request, 'trailHQ/trail_detail.html', {'trail': trail})
+        trail = SingletracksTrail.objects.filter(name__icontains=pk)
+        if trail:
+            print(trail.values())
+            return render(request, 'trailHQ/trail_detail.html', {'trails': trail})
+        else:
+            print("Not found")
     except SingletracksTrail.DoesNotExist:
+        raise Http404("Does not extist")
+        #ST_Helper.makeRequest()
+
 
 
 
@@ -16,4 +24,5 @@ def trail_detail(request, pk):
 
 def all(request):
     trails =  SingletracksTrail.objects.all()
+    print(trails.values())
     return render(request, 'trailHQ/all.html', {'trails': trails})
