00dfdeecd9a243ca6b198ccc47f3dab37495e35f
hwoods01@ksu.edu <hwoods01@ksu.edu>
2017-03-23 12:51:06 -0500

    Started working on api request

--- trailHQ/utils/ST_Helper.py
+++ trailHQ/utils/ST_Helper.py
@@ -1,5 +1,32 @@
+# coding: interpy
+
+import requests
 
 
 
 
-#ef makeRequest(key):
+SingleTracksURL = 'https://trailapi-trailapi.p.mashape.com/'
+key = '"X-Mashape-Key": "Li4lO5svuRmshPnqogrJpkOC4KxAp1kej3ijsnXCRbpxawhJ5f"'
+
+radius = "radius=50"
+activity_string = 'q[activities_activity_type_name_eq]=mountain+biking'
+
+
+country_string = 'q[country_cont]=United States'
+headers = {key, 'Accept": "text/plain'}
+
+
+#https://trailapi-trailapi.p.mashape.com/?lat=34.1&limit=25&lon=-105.2&q[activities_activity_type_name_eq]=mountain+biking&q[city_cont]=Wichita&q[country_cont]=United+States&q[state_cont]=Kansas&radius=50
+
+def makeRequest(city, state):
+    city_string = "q[city_cont]=#{city}"
+    state_string = "q[state_cont]=#{state}"
+    print(city_string)
+    unformreq = SingleTracksURL + '?' + activity_string + '&' + city_string + '&'+ state_string + '&' + country_string +'&' + radius
+
+    print(unformreq)
+
+    r=requests.get(unformreq, headers=headers)
+    print(r)
+
+
--- trailHQ/views.py
+++ trailHQ/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import SingletracksTrail
-#from utils import ST_Helper #THIS WILL BE USED WHEN i HAVE CALLS TO MAKE
+from trailHQ.utils.ST_Helper import makeRequest #THIS WILL BE USED WHEN i HAVE CALLS TO MAKE
 from django.http import Http404
 from django.template import RequestContext
 # Create your views here.
@@ -23,6 +23,7 @@ def trail_detail(request, pk):
 
 
 def all(request):
+    makeRequest('Wichita', 'Kansas')
     trails =  SingletracksTrail.objects.all()
     print(trails.values())
     return render(request, 'trailHQ/all.html', {'trails': trails})
